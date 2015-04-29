0604ec73d4cce283eac6f8f3d63fd83399dacb02
Mirsattar <sattar.seyidov@gmail.com>
2015-04-29 11:19:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,129 +10,174 @@
 #include <pbc.h>
 #include <cstring>
 #include <vector>
-#include "base64.h"
 #include <iostream>
-
+#include <ctime>
+#include <fstream>
 
 using namespace std;
 
 class encryption
 {
 public: 
-    void gen(char generStr[], int lenght,  int l)
+    encryption(char* fileName)
     {
-        memcpy(param, generStr, length);
-        pairing_init_set_buf(pairing, param, lenght);
+        fstream fs;
+        fs.open(fileName, std::fstream::in| std::fstream::binary);
+        size_t count = 1024;
+        fs.read(param, count);
+        
+        pairing_init_set_buf(pairing, param, count);        
+        fs.close();
+    }
+    
+    void set_elemG2(element_t& elem)
+    {
+        element_init_G2(elem, pairing);
+    }
+    void gen(int l)
+    {
+        element_init_G2(H, pairing);
         element_from_hash(H, (void*)"ABCDEF", 6);
         element_init_G1(P, pairing);
+        element_init_G1(Q, pairing);
         element_init_Zr(a, pairing);
-        element_init_G1(secret_key[0], pairing);
+       
+        secret_key = new element_t[1];
+        element_init_G2(secret_key[0], pairing);
         element_random(P);
         element_random(a);
         element_pow_zn(Q, P, a);
-        element_pow_zn(secret_key[0], H, a);
+        
+        element_t tmp;
+        element_init_G2(tmp, pairing);
+        element_mul(secret_key[0], H, a);
+        cipherKeyLength = 1;
     }
     
-    void der(char* name1, int length1, char* name2, int length2, element_t& Sw)
+    void der(string name1, int length1, string name2, int length2, element_t*& Sw0, element_t*& Sw1)
     {
         element_t pw0, pw1, Hw0, Hw1;
         element_init_Zr(pw0, pairing);
         element_init_Zr(pw1, pairing);
         element_random(pw0);
         element_random(pw1);
+
+        element_init_G2(Sw0[0], pairing);
+        element_init_G2(Sw1[0], pairing);
+        element_mul(Sw0[0], P, pw0);
+        element_mul(Sw1[0], P, pw1);
         
-        element_t Rw0, Rw1;
-        element_init_Zr(Rw0, pairing);
-        element_init_Zr(Rw1, pairing);
-        element_pow_zn(Rw0, P, pw0);
-        element_pow_zn(Rw1, P, pw1);
+        element_init_G2(Sw0[1], pairing);
+        element_init_G2(Sw1[1], pairing);
         
-        element_t Sw0, Sw1;
-        element_init_Zr(Sw0, pairing);
-        element_init_Zr(Sw1, pairing);
+        element_init_G2(Hw0, pairing);
+        element_init_G2(Hw1, pairing);
         
-        element_from_hash(Hw0, name1, length1);
-        element_from_hash(Hw1, name2, length2);
+        element_from_hash(Hw0, const_cast<char*>(name1.c_str()), length1);
+        element_from_hash(Hw1, const_cast<char*>(name2.c_str()), length2);
         
         element_t tmp0, tmp1;
-        element_pow_zn(tmp0, Hw0, pw0);
-        element_pow_zn(tmp1, Hw1, pw1);
+        element_init_G2(tmp0, pairing);
+        element_init_G2(tmp1, pairing);
+        element_mul(tmp0, Hw0, pw0);
+        element_mul(tmp1, Hw1, pw1);
+               
+        element_add(Sw0[1], secret_key[0], tmp0);
+        element_add(Sw1[1], secret_key[0], tmp1);
         
-        element_add(Sw0, Sw, tmp0);
-        element_add(Sw1, Sw, tmp1);
-           
+        
+        //element_printf("cipher text 1 :%B \n", Sw0);
+        //element_printf("cipher text 2 :%B \n", Sw1);
     }
     
-    void Enc(long M, int length, std::vector<long>& w)
+    void Enc(std::vector<string>& w, element_t*& cipher_text)
     {
         element_t y;
         element_init_Zr(y, pairing);
         element_random(y);
-        
+
         element_t tmp_w;
+        element_t tmp_w2;
         element_t tmp;
+        element_init_G2(tmp_w, pairing);
+        element_init_G2(tmp_w2, pairing);
+        element_init_G1(tmp, pairing);
+                
+        element_mul(tmp, P, y);
         
-        
-        element_pow_zn(tmp, P, y);
-        element_t* cipher_text = new element_t[w.size()+2];
+        cipher_text = new element_t[w.size()+2];
+        element_init_G1(cipher_text[0], pairing);
         element_set(cipher_text[0], tmp);
-        
         int i;
         for(i =1; i<(w.size()+1); ++i)
         {
+            element_init_G2(cipher_text[i], pairing);
             element_from_hash(tmp_w, &w[i-1], 1);
-            element_pow_zn(tmp, tmp_w, y);
-            element_set(cipher_text[0], tmp);
+            element_mul(tmp_w2, tmp_w, y);
+            element_set(cipher_text[i], tmp_w2);
         }
-        
+        element_init_G2(cipher_text[i], pairing);
         element_t d;
-        element_init_Zr(d, pairing);
-        pairing_pp_t pp;
-        pairing_pp_init(pp, Q, pairing); 
-        pairing_pp_apply(d, H, pp); 
+        element_init_GT(d, pairing);
+        pairing_apply(d, Q, H, pairing); 
+       
+        element_init_G2(symmetricKey, pairing);
+        element_random(symmetricKey);
         
-        element_set_si(tmp, M); 
-        element_mul(tmp, tmp, d);
-        element_set(cipher_text[i], tmp);
-        
+        element_printf("We are going to encrypt a symmetric key for user %s : %B \n", w[0].c_str(), symmetricKey);
         
         
-        pairing_pp_clear(pp);
-        
+        element_mul(tmp_w, d, symmetricKey);
+        element_set(cipher_text[i], tmp_w);
+
     }
     
     
-    void dec(element_t* sk, int cipherKey, std::vector<long>& w, element_t* cipher_text, int cipherLength)
+    void dec(std::vector<string>& w, element_t*& cipher_text, element_t*& secret_key)
     {
+        int cipherLength = w.size()+2;
         element_t d;
         element_t M;
         element_t numerator;
         element_t denumerator;
-        element_init_Zr(numerator, pairing);
-        element_init_Zr(d, pairing);
-        element_init_Zr(denumerator, pairing);
-        element_init_Zr(M, pairing);
-        element_t tmp;
-        pairing_pp_t pp;
-        pairing_pp_init(pp, cipher_text[0], pairing); 
-        pairing_pp_apply(numerator, sk[cipherKey-1], pp);
+        element_init_GT(numerator, pairing);
+        element_init_GT(d, pairing);
+        element_init_GT(denumerator, pairing);
+        mpz_t one;
+        mpz_init (one);
+        element_set_mpz(denumerator, one);
+        element_init_same_as(denumerator, d);
         
+        element_init_G2(M, pairing);
+        element_t tmp;
+        element_init_GT(tmp, pairing);
+        
+        pairing_apply(numerator, Q, H, pairing);
+
         for(int i=1; i<cipherLength-1; ++i)
         {
-            pairing_pp_init(pp, cipher_text[i], pairing); 
-            pairing_pp_apply(tmp, sk[i-1], pp);
+            pairing_apply(tmp, secret_key[i-1], cipher_text[i], pairing);
             element_mul(denumerator, denumerator, tmp);
         }
-        
+ 
+
         element_div(d, numerator, denumerator);
-        element_div(M, cipher_text[cipherLength-1], d);
+        
+        element_div(M, cipher_text[cipherLength-1], numerator);
+
+        element_printf("Decrypted symmetric key for user %s : %B \n", w[0].c_str(), M);
+        
+
     }
-private:
+    
+    element_t symmetricKey;
+    
     pairing_t pairing;
     char param[1024];
     int length;
     element_t P, Q, a, H;
-    std::vector<element_t> secret_key;
+    element_t* secret_key;
+    int cipherKeyLength;
 };
 
 
@@ -140,25 +185,36 @@ private:
 
 int main(int argc, char** argv) {
     
-  
-  
-    string m;
-    cout << "Enter message:" << std::endl;
-    getline(cin, m);
-  std::string encoded = base64_encode(reinterpret_cast<const unsigned char*>(m.c_str()), m.length());
-  std::string decoded = base64_decode(encoded);
-
-  std::cout << "encoded: " << encoded << std::endl;
-  std::cout << "decoded: " << decoded << std::endl;
-
- 
+    encryption* e = new encryption(argv[1]);
+    std::vector<string> names;
     
     pairing_t pairing;
-    char param[1024];
-    size_t count = fread(param, 1, 1024, stdin);
-    if (!count) pbc_die("input error");
-    pairing_init_set_buf(pairing, param, count);
     
+    names.push_back("lol1");
+    names.push_back("lol2");
+  
+
+    element_t* cipher;
+
+    element_t* Sw0 = new element_t[2];
+    element_t* Sw1 = new element_t[2];
+    
+    e->gen(1);
+    e->der(names[0], 4, names[1], 4, Sw0, Sw1);
+    
+    std::vector<string> name1;
+    name1.push_back(names[0]);
+    
+    std::vector<string> name2;
+    name2.push_back(names[1]);
+    
+    e->Enc(name1, cipher);
+    e->dec(name1, cipher, Sw0);
+    
+    e->Enc(name2, cipher);
+    e->dec(name2, cipher, Sw1);
+    
+    delete e;
     return 0;
 }
 
